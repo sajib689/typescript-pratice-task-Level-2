29fc248146aabca67d3c914d5cc35bc8eadbc543
sajib689 <jibonraja11@gmail.com>
2024-10-18 12:59:21 +0600

    filter the data from one array to another array

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@ const uerInfo =[101, 'Ema', 'John', true, '2023']
 const person: [number, string, string, boolean, string] = [101, 'Ema', 'John', true, '2023']
 
 console.log(person[0])
+
+const arr1 = [1, 2, 3, 4, 5]
+const arr2 = [5, 6, 7, 1]
+
+const match = (arr1: number [], arr2: number []) =>{
+    return arr1.filter(item => arr2.includes(item))
+}
+console.log(match(arr1, arr2))
